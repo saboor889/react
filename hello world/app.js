49cb3022ee608b8a904478d5c70201ad4b9c42a5
Syed Haris Ali <o16.haris@gmail.com>
2021-09-03 00:46:51 +0500

    initial commit

---
+++ hello world/app.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+let where = document.getElementById('app')
+
+ReactDOM.render(React.createElement('h1',null,"hello world we are learnin react"), where)
---
+++ hello-world-cli/src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let h1 = React.createElement ('h1',null,'hello world cli');
+
+ReactDOM.render (h1,document.getElementById('root'))
